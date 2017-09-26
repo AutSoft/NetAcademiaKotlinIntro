e8123b0d5960349c5a9fa4755c99acca07e9db98
Péter Ekler <peter.ekler@gmail.com>
2017-09-26 11:43:42 +0200

    Minor changes in TicTactoeView.kt

--- 1_kotlin_tictactoe/TicTacToeKotlin/app/src/main/java/hu/bme/aut/android/tictactoekotlin/view/TicTacToeView.kt
+++ 1_kotlin_tictactoe/TicTacToeKotlin/app/src/main/java/hu/bme/aut/android/tictactoekotlin/view/TicTacToeView.kt
@@ -10,15 +10,14 @@ import android.view.MotionEvent
 import hu.bme.aut.android.tictactoekotlin.data.TicTacToeModel
 
 
-class TicTacToeView : View {
+class TicTacToeView(ctx: Context, attrs: AttributeSet) : View(ctx, attrs) {
 
-    private val paintBg : Paint = Paint()
-    private val paintLine : Paint = Paint()
+    private val paintBg = Paint()
+    private val paintLine = Paint()
 
-    constructor(ctx: Context, attrs: AttributeSet) : super(ctx, attrs) {
+    init {
         paintBg.color = Color.BLACK
         paintBg.style = Paint.Style.FILL
-
         paintLine.color = Color.WHITE
         paintLine.style = Paint.Style.STROKE
         paintLine.strokeWidth = 5f
@@ -47,26 +46,21 @@ class TicTacToeView : View {
                 paintLine)
     }
 
-    private fun drawPlayers(canvas: Canvas?) {
+    private fun drawPlayers(canvas: Canvas) {
         for (i in 0..2) {
             for (j in 0..2) {
                 if (TicTacToeModel.getFieldContent(i, j) == TicTacToeModel.CIRCLE) {
-
-                    // draw a circle at the center of the field
-
-                    // X coordinate: left side of the square + half width of the square
                     val centerX = (i * width / 3 + width / 6).toFloat()
                     val centerY = (j * height / 3 + height / 6).toFloat()
                     val radius = height / 6 - 2
 
-                    canvas?.drawCircle(centerX, centerY, radius.toFloat(), paintLine)
-
+                    canvas.drawCircle(centerX, centerY, radius.toFloat(), paintLine)
                 } else if (TicTacToeModel.getFieldContent(i, j) == TicTacToeModel.CROSS) {
-                    canvas?.drawLine((i * width / 3).toFloat(), (j * height / 3).toFloat(),
+                    canvas.drawLine((i * width / 3).toFloat(), (j * height / 3).toFloat(),
                             ((i + 1) * width / 3).toFloat(),
                             ((j + 1) * height / 3).toFloat(), paintLine)
 
-                    canvas?.drawLine(((i + 1) * width / 3).toFloat(), (j * height / 3).toFloat(),
+                    canvas.drawLine(((i + 1) * width / 3).toFloat(), (j * height / 3).toFloat(),
                             (i * width / 3).toFloat(), ((j + 1) * height / 3).toFloat(), paintLine)
                 }
             }
@@ -75,7 +69,6 @@ class TicTacToeView : View {
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         if (event.action == MotionEvent.ACTION_DOWN) {
-
             val tX = event.x.toInt() / (width / 3)
             val tY = event.y.toInt() / (height / 3)
 
@@ -86,8 +79,6 @@ class TicTacToeView : View {
                 invalidate()
             }
         }
-
         return super.onTouchEvent(event)
     }
-
 }
